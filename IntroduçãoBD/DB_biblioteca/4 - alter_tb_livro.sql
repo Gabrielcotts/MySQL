68dee3670d368a96a5e263c37c7e19ea1893f3bd
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-06-26 16:23:04 -0300

    Update 4 - alter_tb_livro.sql

--- IntroduçãoBD/DB_biblioteca/4 - alter_tb_livro.sql
+++ IntroduçãoBD/DB_biblioteca/4 - alter_tb_livro.sql
@@ -1,4 +1,5 @@
 alter table tb_livro add column editora varchar(80) not null;
 alter table tb_livro modify column editora varchar(80) not null default 'desconhecida';
 alter table tb_livro change column editora nome_editora varchar(80) not null default 'desconhecida';
-alter table tb_livro modify column editora varchar(80) after autor;
+alter table tb_livro modify column nome_editora varchar(80) after autor;
+describe tb_livro;
