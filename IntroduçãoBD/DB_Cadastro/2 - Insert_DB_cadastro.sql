70dd61ba1a64ea82331fd9ddcd5c4d4f2c710605
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-06-27 18:46:34 -0300

    Update 2 - Insert_DB_cadastro.sql

--- IntroduçãoBD/DB_Cadastro/2 - Insert_DB_cadastro.sql
+++ IntroduçãoBD/DB_Cadastro/2 - Insert_DB_cadastro.sql
@@ -10,3 +10,15 @@ insert into tb_pessoa values
  (default,'Gabriel', '2005-02-14', 'M', 92.47 , 1.82, default),
  (null, 'Nathaly', '2006-05-08', 'F', 48.76 , 1.58, 'Cuba'),
  (default, 'Luiza', '1988-09-29', 'F', 68.71 , 1.76, default);*/
+ 
+ insert into cursos values
+('1','HTML4','Curso de HTML5','40','37','2014'),
+('2','Algoritmos','Lógica de Programação','20','15','2014'),
+('3','Photoshop','Dicas de Photoshop CC','10','8','2014'),
+('4','PGP','Curso de PHP para iniciantes','40','20','2010'),
+('5','Jarva','Introdução à Linguagem Java','10','29','2000'),
+('6','MySQL','Bancos de Dados MySQL','30','15','2016'),
+('7','Word','Curso completo de Word','40','30','2016'),
+('8','Sapateado','Danças Rítmicas','40','30','2018'),
+('9','Cozinha Árabe','Aprenda a fazer Kibe','40','30','2018'),
+('10','YouTuber','Gerar polêmica e ganhar inscritos','5','2','2018');
