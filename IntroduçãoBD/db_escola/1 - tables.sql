cadb4d4d65c26609600bab2e2e4313f72f48b58f
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-07-14 13:55:59 -0300

    Add files via upload

---
+++ IntroduçãoBD/db_escola/1 - tables.sql
@@ -0,0 +1,19 @@
+create database db_escola
+default character set utf8mb4
+default collate utf8mb4_0900_ai_ci;
+
+use db_escola;
+
+create table tb_alunos(
+ra int not null auto_increment primary key,
+nome varchar(120) not null,
+nascimento date not null,
+cpf char(11) not null unique,
+altura decimal(3, 2) not null, 
+peso decimal(5, 2) not null,
+nacionalidade varchar(60) default 'Brasil',
+mensalidade decimal(6, 2),
+curso varchar(120) not null,
+sexo enum('M', 'F')
+
+) default char set = utf8mb4;
---
+++ IntroduçãoBD/db_escola/3 - Select.sql
@@ -0,0 +1,9 @@
+select * from tb_alunos;
+
+select nome from tb_alunos;
+select nome from tb_alunos order by nome asc;
+select nome, nascimento from tb_alunos where nascimento between '2000-01-01' and  '2015-12-31' order by nascimento asc;
+select nome, nascimento from tb_alunos where nascimento between '2000-01-01' and  '2015-12-31' order by nome asc;
+select nome, curso, sexo from tb_alunos where curso = 'Engenharia de Software' and sexo = 'M';
+select nome, curso, sexo from tb_alunos where curso = 'Engenharia de Software' and sexo = 'F';
+select nome, nacionalidade from tb_alunos where nacionalidade = 'Brasil' and sexo = 'F' and nome like '%';
