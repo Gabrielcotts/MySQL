8f6952198c02f96196f73f8a0bf5ac3a6ad584fc
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-07-14 19:40:31 -0300

    Update query to filter female Brazilian students by name

--- IntroduçãoBD/db_escola/3 - Select.sql
+++ IntroduçãoBD/db_escola/3 - Select.sql
@@ -6,4 +6,4 @@ select nome, nascimento from tb_alunos where nascimento between '2000-01-01' and
 select nome, nascimento from tb_alunos where nascimento between '2000-01-01' and  '2015-12-31' order by nome asc;
 select nome, curso, sexo from tb_alunos where curso = 'Engenharia de Software' and sexo = 'M';
 select nome, curso, sexo from tb_alunos where curso = 'Engenharia de Software' and sexo = 'F';
-select nome, nacionalidade from tb_alunos where nacionalidade = 'Brasil' and sexo = 'F' and nome like '%';
+select nome, nacionalidade from tb_alunos where nacionalidade = 'Brasil' and sexo = 'F' and nome like 'a%';
